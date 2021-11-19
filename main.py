f2cb4f7d8e5ad46e5ae11fe113034ee48c36701f
Murvoaz <93915219+Murvoaz@users.noreply.github.com>
2021-11-20 00:11:50 +0100

    Version 1.1

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ eel.init('web')
 
 # Exposing the random_python function to javascript
 @eel.expose	
-def random_python():
-	print("Random function running")
-	return randint(1,100)
+def melange(src1,src2,src3):
+        src1 = src1[27:]
+        src2 = src2[27:]
+        src3 = src3[27:]
+        melange = [src1,src2,src3]
+        melange.sort()
+        recette = melange[0]+melange[1]+melange[2]+"\n"
+        filin = open("recette.txt", "r")
+        lignes = filin.readlines()
+        verif = 0
+        element = ""
+        for ligne in lignes:
+                if(verif == 1) :
+                        element = ligne
+                        element = element.strip('\n')
+                        break
+                if(ligne == recette):
+                        verif = 1
+        filin.close()
+        return element
 
 # Start the index.html file
 eel.start("index.html", port = 7998)
